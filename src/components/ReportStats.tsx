60c948b89efd9d6ca7db8300da87f0246d2d1f79
kao <trumblays@yahoo.com>
2022-01-11 15:56:39 -0600

    adding filter

--- src/components/ReportStats.tsx
+++ src/components/ReportStats.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/configureStore';
 
 export default function ReportStats() {
   const bug = useSelector((state: RootState) => state.bug.bug);
+  const [openFE, setOpenFE] = useState(0);
+  const open = bug.filter(
+    (b: { catgetory: string; status: string }) =>
+      b.catgetory === 'Frontend' && b.status === 'Open'
+  );
+  let dcout = open.length;
+  // console.log(open);
+  // setOpenFE(open.length);
+  console.log(bug.length);
   return (
     <div className='w-4/6 text-blue-500 rounded-2xl bg-blue-500 p-2'>
       <div className='flex flex-row justify-between'>
@@ -23,7 +32,8 @@ export default function ReportStats() {
               <td className='pl-5'>Frontend</td>
               <td>9</td>
               <td>20</td>
-              {}
+              {bug ? 'hello' : 'loading...'}
+              {openFE}
               <td>40</td>
               <td>3943</td>
             </tr>
