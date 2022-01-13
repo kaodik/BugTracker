222abe66752c061e7666f2bc556c537aaf853843
kao <trumblays@yahoo.com>
2022-01-13 13:14:37 -0600

    addined filter

--- src/components/BugStatus.tsx
+++ src/components/BugStatus.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/configureStore';
 
 export default function BugStatus() {
+  const bug = useSelector((state: RootState) => state.bug.bug);
+  const bugNameList = bug.filter(
+    (b: { assignee: string }) => b.assignee === 'Steven Trumblay'
+  );
   return (
     <div className='w-[450px] text-blue-500 rounded-2xl bg-blue-500 p-2'>
       <div className='flex flex-row justify-between'>
