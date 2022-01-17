5412ae577be754ca64e2283db28f00abfb17becb
kao <trumblays@yahoo.com>
2022-01-17 10:14:04 -0600

    Setting up login form and page as default

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,40 +1,40 @@
-import React from "react";
+import React from 'react';
 
 export default function LoginForm() {
   return (
-    <div className="ml-96 w-96 rounded-2xl shadow-lg shadow-blue-800">
+    <div className='ml-96 w-96 rounded-2xl shadow-lg shadow-blue-800'>
       <form
-        action=""
-        className="w-96  text-center rounded-xl p-3 flex flex-col  bg-transparent shadow-inner shadow-blue-300"
+        action=''
+        className='w-96  text-center rounded-xl p-3 flex flex-col  bg-transparent shadow-inner shadow-blue-300'
       >
-        <span className="text-blue-600 pb-2">Login Form</span>
-        <div className="space-y-2">
+        <span className='text-blue-600 pb-2'>Login Form</span>
+        <div className='space-y-2'>
           <input
-            type="text"
-            placeholder="Username"
-            className="placeholder-blue-600 rounded-lg text-center w-11/12 shadow-inner shadow-black bg-blue-300 focus:outline-none focus:ring focus:ring-transparent  "
+            type='text'
+            placeholder='Username'
+            className='placeholder-blue-600 rounded-lg text-center w-11/12 shadow-inner shadow-black bg-blue-300 focus:outline-none focus:ring focus:ring-transparent  '
           />
           <input
-            type="text"
-            placeholder="Password"
-            className="placeholder-blue-600 rounded-lg text-center  w-11/12 shadow-inner shadow-black bg-blue-300 focus:outline-none focus:ring focus:ring-transparent"
+            type='text'
+            placeholder='Password'
+            className='placeholder-blue-600 rounded-lg text-center  w-11/12 shadow-inner shadow-black bg-blue-300 focus:outline-none focus:ring focus:ring-transparent'
           />
         </div>
-        <div className="p-4 flex flex-col">
+        <div className='p-4 flex flex-col'>
           <a
-            href="#"
-            className="text-base text-left focus:outline-none focus:ring focus:ring-transparent"
+            href='#'
+            className='text-base text-left focus:outline-none focus:ring focus:ring-transparent'
           >
             Forgot Password
           </a>
           <a
-            href="#"
-            className="text-base text-left focus:outline-none focus:ring focus:ring-transparent"
+            href='#'
+            className='text-base text-left focus:outline-none focus:ring focus:ring-transparent'
           >
-            Register
+            Registration
           </a>
         </div>
-        <button className=" focus:outline-none focus:ring focus:ring-transparent rounded-2xl text-base m-auto hover:shadow-blue-600 w-4/12 shadow-inner shadow-blue-400 bg-blue-500 p-2">
+        <button className=' focus:outline-none focus:ring focus:ring-transparent rounded-2xl text-base m-auto hover:shadow-blue-600 w-4/12 shadow-inner shadow-blue-400 bg-blue-500 p-2'>
           Submit
         </button>
       </form>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React from 'react';
+import LoginForm from '../components/LoginForm';
 
 export default function Login() {
   return (
-    <div>
-      <h1>login page</h1>
+    <div className='pl-28 pt-40'>
+      <LoginForm />
     </div>
   );
 }
