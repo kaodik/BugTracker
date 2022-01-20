3913be04914a4fd43ff50e97e0186f5dfcd287c0
kao <trumblays@yahoo.com>
2022-01-20 08:50:37 -0600

    rm unused code.

--- src/redux/sagas/request/account.js
+++ src/redux/sagas/request/account.js
@@ -4,3 +4,6 @@ export function requestGetAccount(account) {
   const body = account.data;
   return axios.post('http://localhost:5000/currentUser', body);
 }
+export function requestUpdateAccount(account) {
+  return axios.post();
+}
