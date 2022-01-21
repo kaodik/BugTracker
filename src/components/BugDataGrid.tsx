3ac79adfbbdd6cec89fd60faf790166a2943702b
kao <trumblays@yahoo.com>
2022-01-21 14:38:03 -0600

    edited design

--- src/components/BugDataGrid.tsx
+++ src/components/BugDataGrid.tsx
@@ -43,26 +43,10 @@ export default function BugDataGrid() {
         </table>
       </div>
       <div className=' rounded-b-2xl bg-blue-300  px-4 py-3 flex items-center justify-between border-t border-blue-300 sm:px-6'>
-        <div className='flex-1 flex justify-between sm:hidden'>
-          <a
-            href='#'
-            className='relative inline-flex items-center px-4 py-2 border border-blue-300 text-sm font-medium rounded-md text-blue-700 bg-white hover:bg-blue-50'
-          >
-            Previous
-          </a>
-          <a
-            href='#'
-            className='ml-3 relative inline-flex items-center px-4 py-2 border border-blue-300 text-sm font-medium rounded-md text-blue-700 bg-white hover:bg-blue-50'
-          >
-            Next
-          </a>
-        </div>
         <div className='hidden sm:flex-1 sm:flex sm:items-center sm:justify-between'>
           <div>
             <p className='text-sm text-blue-700'>
-              Showing <span className='font-medium'>1</span> to{' '}
-              <span className='font-medium'>10</span> of{' '}
-              <span className='font-medium'>97</span> results
+              Showing <span className='font-medium'>{bug.length}</span> results
             </p>
           </div>
           {/* <div>
